571252617a65bfe6fde5a5fc3590dce7f789b658
Cesar Quilodran-Casas <cesar.quilodran.casas@gmail.com>
2025-01-28 12:02:41 +0000

    rebase

---
+++ embeddingModelsList.py
@@ -0,0 +1,10 @@
+embeddings_model_names = [
+    'nvidia/NV-Embed-2',
+    'sentence-transformers/all-MiniLM-L6-v2',
+    'allenai/specter2_base',
+    'dunzhang/stella_en_1.5B_v5',
+    'ClimateBERT/netzero-reduction',
+    'ClimateBERT/distilroberta-base-climate-detector'
+    'jxm/cde-small-V2'
+]
+
